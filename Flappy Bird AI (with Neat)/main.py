9f539a83a80f5d015a1c1c7147b452808c2ff76d
User-92 <51941595+User-92@users.noreply.github.com>
2020-07-03 11:35:52 -0500

    Update main.py

--- Flappy Bird AI (with Neat)/main.py
+++ Flappy Bird AI (with Neat)/main.py
@@ -1,3 +1,4 @@
+# /usr/bin/python3.7
 # Setup Pygame and imports --------------------------------------------------------------------------------------------- #
 import pygame, sys, random, os, pickle,neat
 import data.font as f
